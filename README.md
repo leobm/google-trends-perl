74d92505699f05398b3f7e4a975283937b574275
Jan-Felix Wittmann <jfwittmann@posteo.net>
2023-08-18 02:12:20 +0200

    initial import

---
+++ README.md
@@ -0,0 +1,23 @@
+
+# NAME
+
+Google::Trends - It's new $module
+
+# SYNOPSIS
+
+    use Google::Trends;
+
+# DESCRIPTION
+
+Google::Trends is ...
+
+# LICENSE
+
+Copyright (C) Jan-Felix Wittmann.
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+# AUTHOR
+
+Jan-Felix Wittmann <jfwittmann@posteo.net>
---
+++ lib/Google/Trends.pm
@@ -0,0 +1,39 @@
+package Google::Trends;
+use 5.008001;
+use strict;
+use warnings;
+
+our $VERSION = "0.01";
+
+
+
+1;
+__END__
+
+=encoding utf-8
+
+=head1 NAME
+
+Google::Trends - It's new $module
+
+=head1 SYNOPSIS
+
+    use Google::Trends;
+
+=head1 DESCRIPTION
+
+Google::Trends is ...
+
+=head1 LICENSE
+
+Copyright (C) Jan-Felix Wittmann.
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=head1 AUTHOR
+
+Jan-Felix Wittmann E<lt>jfwittmann@posteo.netE<gt>
+
+=cut
+
